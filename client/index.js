c745dfc2b98c3f33848ef2939703ea1d2986eeb8
Christian van Drunen <christian.van.drunen@hva.nl>
2025-04-24 01:35:27 +0200

    code cleaning and sources?

--- client/index.js
+++ client/index.js
@@ -8,16 +8,15 @@ console.log('Hello, world!');
 //   });
 
 document.addEventListener("DOMContentLoaded", () => {
-    function DefaultAnimation(){
-      const cards = document.getElementsByClassName("card");
-      console.log("Cards found:", cards.length);
- 
-      let id = 1;
-      for (let card of cards){
+  function DefaultAnimation() {
+    const cards = document.getElementsByClassName("card");
+
+    let id = 1;
+    for (let card of cards) {
       card.animate(
         [
           { transform: "translateY(-5px)" },
-        { transform: "translateY(5px)" },
+          { transform: "translateY(5px)" },
         ],
         {
           duration: 1000,
@@ -29,103 +28,103 @@ document.addEventListener("DOMContentLoaded", () => {
       );
       id += 1;
     }
-}
-    DefaultAnimation();
-  });
+  }
+  DefaultAnimation();
+});
 
 
-  battlebutton.addEventListener('click', () => {
-console.log('KIIIPP');
-BattleAnimation();
-  });
+battlebutton.addEventListener('click', () => {
+  console.log('KIIIPP');
+  BattleAnimation();
+});
 
 
-function BattleAnimation(){
+function BattleAnimation() {
   const cards = document.getElementsByClassName("card");
   const cardsContent = document.getElementsByClassName("card-content");
 
-  
+
   cardsContent[0].animate(
-      [
-        { transform: `rotateY(deg)`, },
-      { transform: `rotateY(45deg)`, offset: .05},
-      { transform: `rotateY(45deg)`, offset: .20},
-      { transform: `rotateY(-45deg)`, offset: .35},
-      { transform: `rotateY(45deg)`, offset: .8},
-      {transform: `rotateY(2turn)`, offset: 1},
-      ],
-      {
-        duration: 4000,
-        delay: 0,
-        iterations: 1,
-        direction: "alternate",
-        easing: "ease-in-out",
-      }
-    );
-  
-    cards[0].animate(
-      [
-        { transform: "translateX(0dvw)", offset: .05 },
+    [
+      { transform: `rotateY(deg)`, },
+      { transform: `rotateY(45deg)`, offset: .05 },
+      { transform: `rotateY(45deg)`, offset: .20 },
+      { transform: `rotateY(-45deg)`, offset: .35 },
+      { transform: `rotateY(45deg)`, offset: .8 },
+      { transform: `rotateY(2turn)`, offset: 1 },
+    ],
+    {
+      duration: 4000,
+      delay: 0,
+      iterations: 1,
+      direction: "alternate",
+      easing: "ease-in-out",
+    }
+  );
+
+  cards[0].animate(
+    [
+      { transform: "translateX(0dvw)", offset: .05 },
       { transform: "translateX(30dvw)", offset: .15 },
       { transform: "translateX(30dvw)", offset: .20 },
-      { transform: "translateX(25dvw", offset: .30},
-      { transform: "translateX(30dvw", offset: .35},
-      { transform: "translateX(20dvw", offset: .45},
-      { transform: "translateX(25dvw", offset: .55},
-      { transform: "translateX(25dvw", offset: .6},
-      { transform: "translateX(15dvw", offset: .75},
-      { transform: "translateX(15dvw", offset: .8},
-      { transform: "translateX(-20dvw", offset: 1},
-      ],
-      {
-        duration: 4000,
-        delay: 0,
-        iterations: 1,
-        direction: "alternate",
-        easing: "ease-in-out",
-      }
-    );
+      { transform: "translateX(25dvw", offset: .30 },
+      { transform: "translateX(30dvw", offset: .35 },
+      { transform: "translateX(20dvw", offset: .45 },
+      { transform: "translateX(25dvw", offset: .55 },
+      { transform: "translateX(25dvw", offset: .6 },
+      { transform: "translateX(15dvw", offset: .75 },
+      { transform: "translateX(15dvw", offset: .8 },
+      { transform: "translateX(-20dvw", offset: 1 },
+    ],
+    {
+      duration: 4000,
+      delay: 0,
+      iterations: 1,
+      direction: "alternate",
+      easing: "ease-in-out",
+    }
+  );
 
-    cardsContent[1].animate(
-      [
-        { transform: `rotateY(deg)`, },
-      { transform: `rotateY(-45deg)`, offset: .05},
-      { transform: `rotateY(-45deg)`, offset: .20},
-      { transform: `rotateY(45deg)`, offset: .35},
-      { transform: `rotateY(-45deg)`, offset: .8},
-      {transform: `rotateY(-2turn)`, offset: 1},
-      ],
-      {
-        duration: 4000,
-        delay: 0,
-        iterations: 1,
-        direction: "alternate",
-        easing: "ease-in-out",
-      }
-    );
+  cardsContent[1].animate(
+    [
+      { transform: `rotateY(deg)`, },
+      { transform: `rotateY(-45deg)`, offset: .05 },
+      { transform: `rotateY(-45deg)`, offset: .20 },
+      { transform: `rotateY(45deg)`, offset: .35 },
+      { transform: `rotateY(-45deg)`, offset: .8 },
+      { transform: `rotateY(-2turn)`, offset: 1 },
+    ],
+    {
+      duration: 4000,
+      delay: 0,
+      iterations: 1,
+      direction: "alternate",
+      easing: "ease-in-out",
+    }
+  );
 
-    cards[1].animate(
-      [
-        { transform: "translateX(0dvw)", offset: .05 },
+  cards[1].animate(
+    [
+      { transform: "translateX(0dvw)", offset: .05 },
       { transform: "translateX(-30dvw)", offset: .15 },
       { transform: "translateX(-30dvw)", offset: .20 },
-      { transform: "translateX(-25dvw", offset: .30},
-      { transform: "translateX(-30dvw", offset: .35},
-      { transform: "translateX(-20dvw", offset: .45},
-      { transform: "translateX(-25dvw", offset: .55},
-      { transform: "translateX(-25dvw", offset: .6},
-      { transform: "translateX(-15dvw", offset: .75},
-      { transform: "translateX(-15dvw", offset: .8},
-      { transform: "translateX(20dvw", offset: 1},
-      ],
-      {
-        duration: 4000,
-        delay: 0,
-        iterations: 1,
-        direction: "alternate",
-        easing: "ease-in-out",
-      }
-    );
+      { transform: "translateX(-25dvw", offset: .30 },
+      { transform: "translateX(-30dvw", offset: .35 },
+      { transform: "translateX(-20dvw", offset: .45 },
+      { transform: "translateX(-25dvw", offset: .55 },
+      { transform: "translateX(-25dvw", offset: .6 },
+      { transform: "translateX(-15dvw", offset: .75 },
+      { transform: "translateX(-15dvw", offset: .8 },
+      { transform: "translateX(20dvw", offset: 1 },
+    ],
+    {
+      duration: 4000,
+      delay: 0,
+      iterations: 1,
+      direction: "alternate",
+      easing: "ease-in-out",
+    }
+  );
 
- 
+
 }
